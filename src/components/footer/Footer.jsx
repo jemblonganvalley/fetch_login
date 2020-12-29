68aa2e3595e9fb7c0205e31e743af817ea8052e4
jemblonganvalley <jemblonganvalley@gmail.com>
2020-12-29 20:26:10 +0700

    update

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,21 @@
+//teman teman import kebutuhan untuk komponent kita
+import "./Footer.css";
+
+//kita buat componentnya
+const Footer = () => {
+  return (
+    <>
+      <section>
+        <h1>hello</h1>
+      </section>
+
+      <div>
+        <h1>ini bundle lainya</h1>
+      </div>
+    </>
+  );
+};
+
+//kita export component agar bisa di import / di gunakan
+//di component lain
+export default Footer;

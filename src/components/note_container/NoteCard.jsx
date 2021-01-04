ab348af168eaab408f79735ffc3a3b281d86589f
jemblonganvalley <jemblonganvalley@gmail.com>
2021-01-04 23:45:28 +0700

    update

--- src/components/note_container/NoteCard.jsx
+++ src/components/note_container/NoteCard.jsx
@@ -4,6 +4,11 @@ import "./NoteCard.css";
 const NoteCard = ({ id, title, body }) => {
   //kita buat togler view untuk form edit
   const [showEditForm, setShowEditForm] = useState(false);
+  const [defaultValue, setDefaultValue] = useState({
+    id: id,
+    title: title,
+    body: body,
+  });
 
   const handleDelete = (args) => {
     fetch(`http://localhost:5000/notes/${args}`, {
@@ -14,6 +19,26 @@ const NoteCard = ({ id, title, body }) => {
       },
     });
 
+    window.location.href = "/home";
+  };
+
+  //fungsi untuk store ke database
+  const handleEdit = (e) => {
+    //agar url tetap
+    e.preventDefault();
+    //fetch
+    fetch(`http://localhost:5000/notes/${id}`, {
+      method: "PATCH",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title: defaultValue.title,
+        body: defaultValue.body,
+      }),
+    });
+    //buat oage me refresh
     window.location.href = "/home";
   };
 
@@ -33,9 +58,36 @@ const NoteCard = ({ id, title, body }) => {
       ></i>
 
       {showEditForm ? (
-        <form>
-          <input type="text" id="title" name="title" />
-          <textarea name="body" id="body" cols="30" rows="10"></textarea>
+        <form className="form_edit" onSubmit={handleEdit}>
+          <label htmlFor="title">judul</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={defaultValue.title}
+            onChange={(e) => {
+              // e.preventDefault();
+              setDefaultValue({
+                ...defaultValue,
+                title: e.target.value,
+              });
+            }}
+          />
+
+          <label htmlFor="body">isi note</label>
+          <textarea
+            name="body"
+            id="body"
+            value={defaultValue.body}
+            onChange={(e) => {
+              // e.preventDefault();
+              setDefaultValue({
+                ...defaultValue,
+                body: e.target.value,
+              });
+            }}
+          ></textarea>
+
           <button type="submit">simpan</button>
         </form>
       ) : (
